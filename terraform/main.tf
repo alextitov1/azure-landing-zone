57fd55bc5a104968a85a4b9396c83ac3e48cc0fa
AlexanderT <alexander.titov@asb.co.nz>
2021-10-27 17:54:39 +1300

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,99 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "TFR_TEST_Lab01"
+  location = "australiaeast"
+}
+
+resource "azurerm_virtual_network" "network" {
+  name                = "vnet01"
+  address_space       = ["192.168.1.0/24"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "bastionsubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["192.168.1.224/27"]
+}
+
+resource "azurerm_public_ip" "bastionpublicip" {
+  name                = "PubIP01-bastion"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  name                = "bastion01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.bastionsubnet.id
+    public_ip_address_id = azurerm_public_ip.bastionpublicip.id
+  }
+}
+
+##### virtual machine
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal01"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["192.168.1.0/27"]
+}
+
+resource "azurerm_network_interface" "jumphost-nic01" {
+  name                = "jumphost-nic01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "jumphost" {
+  name                = "jumphost01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2_v3"
+  admin_username      = "alex"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.jumphost-nic01.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+}
+
+variable "resource_group_name" {
+  description = "The name of the resource group"
+}
+
+variable "admin_username" {
+  description = "Username for the Domain Administrator user"
+}
+
+variable "admin_password" {
+  description = "Password for the Adminstrator user"
+}
