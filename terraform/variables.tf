129c37e450a5291930c5181b24c23547f47e0f19
AlexanderT <alexander.titov@asb.co.nz>
2021-10-27 18:36:42 +1300

    4th commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,16 +4,20 @@ variable "prefix" {
 
 variable "location" {
   description = "The Azure Region in which all resources in this example should be created."
+  default = "australiaeast"
 }
 
 variable "resource_group_name" {
   description = "The name of the resource group"
+  default = "testing"
 }
 
 variable "admin_username" {
   description = "Username for the Domain Administrator user"
+  default = "admin1"
 }
 
 variable "admin_password" {
   description = "Password for the Adminstrator user"
+  default = "password1"
 }
