2e69c51e1cc16f365ea8327a086cebca3d426a17
alextitov1 <ae.titov@ya.ru>
2021-10-28 11:13:22 +0000

    change vm image version

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ resource "azurerm_windows_virtual_machine" "jumphost" {
   source_image_reference {
     publisher = "MicrosoftWindowsServer"
     offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
+    sku       = "2019-Datacenter"
     version   = "latest"
   }
 }
