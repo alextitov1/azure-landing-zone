2cb8321aae5ee18ed044b3bff853e159eba48c7e
alextitov1 <42015541+alextitov1@users.noreply.github.com>
2023-03-15 11:22:19 +0000

    azure pipeline draft

--- ESLZ/terraform/network/firewallpolicycollection/main.tf
+++ ESLZ/terraform/network/firewallpolicycollection/main.tf
@@ -1,7 +1,7 @@
 
 
 module "azurefirewallpolicyrulecollectiongroup" {
-    source = "../modules/network/azurefirewallpolicyrulecollectiongroup"
+    source = "../../modules/network/azurefirewallpolicyrulecollectiongroup"
     name                            = "tf_policy_collection01"
     priority                        = 103
     firewall_policy_id              = data.terraform_remote_state.firewall.outputs.azurefirewallbasepolicy_id
